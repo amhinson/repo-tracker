9e54982a0c84e9489f0933886dbee6586f2b43cc
Alex Hinson <alexmhinson@gmail.com>
2025-05-01 21:41:01 -0400

    Github Authentication (#1)

--- client/lib/apolloClient.tsx
+++ client/lib/apolloClient.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { SessionProvider } from 'next-auth/react';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -8,5 +9,9 @@ const client = new ApolloClient({
 });
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return (
+    <SessionProvider>
+      <ApolloProvider client={client}>{children}</ApolloProvider>
+    </SessionProvider>
+  );
 }
